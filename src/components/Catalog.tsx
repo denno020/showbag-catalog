f26d021d73fce39ecb08e720025c1c0a4df6d711
Luke Denton <denno020@gmail.com>
2024-08-19 16:19:25 +0930

    Remove left over console log

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -5,7 +5,6 @@ import Card from './Card'
 const Catalog = (props: { items: ShowbagItem[] }) => {
   const { items } = props;
   const [location, setLocation] = useLocation();
-  console.log({ location })
 
   return items.map((item) => <Card item={item}/>)
 }
