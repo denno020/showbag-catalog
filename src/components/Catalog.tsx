7b398c68fb623115cccab4903c1f643dde6523ac
Luke Denton <denno020@gmail.com>
2024-08-20 20:36:09 +0930

    Auto format the Catalog component
    This looks much better

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,19 +1,23 @@
-import {useLocation, Router} from 'wouter';
-import type {ShowbagItem} from '../showbags'
-import Card from './Card'
+import type { ShowbagItem } from '../showbags';
+import Card from './Card';
 
 type CatalogProps = {
-  items: ShowbagItem[],
-  onToggleInShoppingBag: (itemSlug: ShowbagItem['slug']) => void,
-  shoppingBagItems: ShowbagItem['slug'][]
-}
+  items: ShowbagItem[];
+  onToggleInShoppingBag: (itemSlug: ShowbagItem['slug']) => void;
+  shoppingBagItems: ShowbagItem['slug'][];
+};
 
 const Catalog = (props: CatalogProps) => {
-  const {items, onToggleInShoppingBag, shoppingBagItems} = props;
-  const [location, setLocation] = useLocation();
+  const { items, onToggleInShoppingBag, shoppingBagItems } = props;
 
-  return items.map((item) => <Card key={item.id} item={item} onToggleInShoppingBag={onToggleInShoppingBag}
-                                   isInBag={shoppingBagItems.includes(item.slug)}/>)
-}
+  return items.map((item) => (
+    <Card
+      key={item.id}
+      item={item}
+      onToggleInShoppingBag={onToggleInShoppingBag}
+      isInBag={shoppingBagItems.includes(item.slug)}
+    />
+  ));
+};
 
 export default Catalog;
