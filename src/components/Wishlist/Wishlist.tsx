02a285d7e8a3743470438afd93fb5785a5d7648c
Luke Denton <denno020@gmail.com>
2024-08-20 20:35:18 +0930

    Change bag icon, update UI for wishlist panel
    I'm not in love with the "close" button used for the wishlist panel, but it'll do for now

--- src/components/Wishlist/Wishlist.tsx
+++ src/components/Wishlist/Wishlist.tsx
@@ -1,24 +1,24 @@
 import { useCallback, useRef } from 'react';
 import { useOutsideClick } from '@chakra-ui/react-use-outside-click';
-import WishlistItem from './WishlistItem.tsx'
+import WishlistItem from './WishlistItem.tsx';
 import { useEscapeKey } from '../../hooks/useEscapeKey.ts';
-import type {ShowbagItem} from "../../showbags.ts";
+import type { ShowbagItem } from '../../showbags.ts';
 import classes from './Wishlist.module.css';
 
 type WishlistProps = {
-  items: ShowbagItem[]
-  onRemove: (slug: ShowbagItem['slug']) => void
-}
+  items: ShowbagItem[];
+  onRemove: (slug: ShowbagItem['slug']) => void;
+};
 
 const Wishlist = (props: WishlistProps) => {
   const wishlistPanel = useRef(null);
   const { items, onRemove } = props;
 
-  const closeWishlist = useCallback(() => {
+  const closeWishlist = useCallback(( ) => {
     history.back();
   }, []);
 
-  useEscapeKey(closeWishlist)
+  useEscapeKey(closeWishlist);
 
   useOutsideClick({
     ref: wishlistPanel,
@@ -29,23 +29,37 @@ const Wishlist = (props: WishlistProps) => {
 
   return (
     <div className={`z-50 ${classes.overlay}`}>
-      <div
-        ref={wishlistPanel}
-        className={`fixed top-0 right-0 z-50 h-full bg-white shadow-lg ${classes.wishlist}`}>
-          <button onClick={closeWishlist}>
-            Close
-          </button>
-        <div className="px-6 py-4">
-          <h2 className="text-lg font-semibold">Show Bags Wish List</h2>
-          <div className="flex flex-col gap-4">
-            {items.map(item => (
+      <div ref={wishlistPanel} className={`fixed top-0 right-0 z-50 h-full bg-white shadow-lg ${classes.wishlist}`}>
+        <button onClick={closeWishlist} className={classes.closeBtn}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <rect width="18" height="18" x="3" y="3" rx="2" />
+            <path d="M15 3v18" />
+            <path d="m8 9 3 3-3 3" />
+          </svg>
+
+          <span className="sr-only">Close</span>
+        </button>
+        <div className="px-6 py-3">
+          <h2 className="text-xl font-semibold text-center">Show Bags Wish List</h2>
+          <div className="flex flex-col gap-4 py-5">
+            {items.map((item) => (
               <WishlistItem key={item.slug} item={item} onRemove={onRemove} />
             ))}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Wishlist;
