7e0527512f32ebf549dcf09bdb5bc975e15bfae3
Luke Denton <denno020@gmail.com>
2024-08-23 12:05:05 +0930

    Make Search auto focus and esc key can cancel search input

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -27,6 +27,12 @@ const Search = () => {
     setQueryParam([{ name: 'search', value }], 'replace');
   };
 
+  const handleCancelSearch = () => {
+    setSearchText('');
+    setQueryParam([{ name: 'search', value: null }], 'replace');
+    setIsSearchVisible(false);
+  };
+
   return (
     <div>
       {!isSearchVisible && (
@@ -45,10 +51,11 @@ const Search = () => {
       {isSearchVisible && (
         <input
           value={searchText}
+          autoFocus={true}
           type="text"
-          id="small-input"
           className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           onChange={(e) => handleSearchInput(e.target.value)}
+          onKeyDown={(e) => e.key === 'Escape' && handleCancelSearch()}
         ></input>
       )}
     </div>
