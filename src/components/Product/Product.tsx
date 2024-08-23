bd9d3541ab7f2cfe8f1900cadd9e8502597dcf8a
Luke Denton <denno020@gmail.com>
2024-08-23 12:06:03 +0930

    Move Product close button to the top of DOM in component
    This is so that it gets focus first, within the focus trap, meaning that pressing TAB once, will actually highlight the first action in the modal, rather than go straight to the close button

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -27,6 +27,18 @@ const Product = (props: ProductProps) => {
             </div>
           </div>
           <div className="flex flex-col gap-4">
+            <div>
+              <button onClick={close} className={classes.closeContainer}>
+                <svg width="30" height="30" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M8 8.707l3.646 3.647.708-.707L8.707 8l3.647-3.646-.707-.708L8 7.293 4.354 3.646l-.707.708L7.293 8l-3.646 3.646.707.708L8 8.707z"
+                  />
+                </svg>
+                <span className="sr-only">close</span>
+              </button>
+            </div>
             <div className="text-2xl">{item.title}</div>
             <div className="text-3xl">${item.showbag_price}</div>
             <div>
@@ -56,18 +68,6 @@ const Product = (props: ProductProps) => {
                 {isInList ? 'Remove from' : 'Add to'}&nbsp; List
               </button>
             </div>
-          </div>
-          <div>
-            <button onClick={close} className={classes.closeContainer}>
-              <svg width="30" height="30" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M8 8.707l3.646 3.647.708-.707L8.707 8l3.647-3.646-.707-.708L8 7.293 4.354 3.646l-.707.708L7.293 8l-3.646 3.646.707.708L8 8.707z"
-                />
-              </svg>
-              <span className="sr-only">close</span>
-            </button>
           </div>
         </div>
       </FocusTrap>
