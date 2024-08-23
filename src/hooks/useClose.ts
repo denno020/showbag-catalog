71cec692979a115cc68c788021615d2c592623f3
Luke Denton <denno020@gmail.com>
2024-08-24 07:55:50 +0930

    Update useClose to handle if a user comes direct into /list
    In this case, the back button won't work, so we need to push a state update which takes the user back to the home page. Using the presence of a search string to indicate that

--- src/hooks/useClose.ts
+++ src/hooks/useClose.ts
@@ -1,11 +1,18 @@
 import { useCallback, useRef } from 'react';
 import { useOutsideClick } from '@chakra-ui/react-use-outside-click';
+import { useLocation, useSearch } from 'wouter';
 import { useEscapeKey } from './useEscapeKey';
 
 export const useClose = () => {
   const ref = useRef(null);
+  const searchParam = useSearch();
+  const [, setLocation] = useLocation();
 
   const close = useCallback(() => {
+    if (searchParam) {
+      setLocation('/');
+      return;
+    }
     history.back();
   }, []);
 
