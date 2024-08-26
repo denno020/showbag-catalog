4a1adf36684fadba292f49d7c5ed727cb495d976
Luke Denton <denno020@gmail.com>
2024-08-26 23:49:06 +0930

    Make the display of the List view a little nicer

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import classnames from 'classnames';
 import toast from 'react-hot-toast';
+import { Input } from 'react-daisyui';
 import ListItem from './ListItem.tsx';
 import { useClose } from '../../hooks/useClose.ts';
 import { useClipboard } from '../../hooks/useClipboard.js';
@@ -62,50 +63,72 @@ const List = (props: ListProps) => {
   };
 
   return (
-    <div className={classes.list}>
-      <header className={classes.header}>
-        <button onClick={handlePrepareShareURL} className={classnames(classes.btn, classes.shareBtn)}>
-          <svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30" fill="currentColor">
-            <path d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-640q17 0 28.5-11.5T760-760q0-17-11.5-28.5T720-800q-17 0-28.5 11.5T680-760q0 17 11.5 28.5T720-720ZM240-440q17 0 28.5-11.5T280-480q0-17-11.5-28.5T240-520q-17 0-28.5 11.5T200-480q0 17 11.5 28.5T240-440Zm480 280q17 0 28.5-11.5T760-200q0-17-11.5-28.5T720-240q-17 0-28.5 11.5T680-200q0 17 11.5 28.5T720-160Zm0-600ZM240-480Zm480 280Z" />
-          </svg>
-
-          <span className="text-sm">Share</span>
-        </button>
-        <div className="flex justify-center gap-2">
-          <h2 className="text-xl font-semibold text-center">
-            {userName === 'My' ? 'My' : `${userName}'s`} Show Bags List
-          </h2>
-          <div>
-            {!isEditingName && (
-              <button onClick={handleChangeName}>{userName === 'My' ? 'Set' : 'Change'}&nbsp;Name</button>
-            )}
-            {isEditingName && (
-              <form onSubmit={saveName}>
-                <input
-                  autoFocus={true}
-                  type="text"
-                  className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  onChange={(e) => setName(e.target.value)}
-                ></input>
-                <button>Save</button>
-                <button onClick={cancelChangeName} type="button">
-                  Cancel
+    <section className={classes.list}>
+      {items.length > 0 && (
+        <header className={classes.header}>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <div className="w-full text-center">
+              {!isEditingName && (
+                <button onClick={handleChangeName} className="btn btn-xs btn-outline">
+                  {userName === 'My' ? 'Set' : 'Change'}&nbsp;Your Name
                 </button>
-              </form>
-            )}
+              )}
+              {isEditingName && (
+                <form onSubmit={saveName}>
+                  <Input
+                    autoFocus={true}
+                    type="text"
+                    size="sm"
+                    placeholder="Enter your name"
+                    className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  ></Input>
+                  <div className="join flex w-full mt-1">
+                    <button className="btn btn-sm join-item flex-1">Save</button>
+                    <button className="btn btn-sm join-item flex-1" onClick={cancelChangeName} type="button">
+                      Cancel
+                    </button>
+                  </div>
+                </form>
+              )}
+            </div>
+            <h2 className="text-xl font-semibold text-center">
+              {userName === 'My' ? 'My' : `${userName}'s`} Show Bags List
+            </h2>
           </div>
-        </div>
-      </header>
-      <div className="px-6 py-3">
-        <div className="flex flex-col gap-4">
-          {items.length === 0 && <p>Nothing added to your list!</p>}
+        </header>
+      )}
+      <div className="px-6">
+        {items.length > 0 && (
+          <div className="flex justify-center">
+            <button onClick={handlePrepareShareURL} className={classnames('btn btn-sm btn-primary', classes.shareBtn)}>
+              <span>Share List</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="20"
+                viewBox="0 -960 960 960"
+                width="20"
+                fill="currentColor"
+              >
+                <path d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-640q17 0 28.5-11.5T760-760q0-17-11.5-28.5T720-800q-17 0-28.5 11.5T680-760q0 17 11.5 28.5T720-720ZM240-440q17 0 28.5-11.5T280-480q0-17-11.5-28.5T240-520q-17 0-28.5 11.5T200-480q0 17 11.5 28.5T240-440Zm480 280q17 0 28.5-11.5T760-200q0-17-11.5-28.5T720-240q-17 0-28.5 11.5T680-200q0 17 11.5 28.5T720-160Zm0-600ZM240-480Zm480 280Z" />
+              </svg>
+            </button>
+          </div>
+        )}
+        <div className={classnames('my-4', classes.listItems)}>
+          {items.length === 0 && <p className="py-5">Nothing added to your list!</p>}
           {items.map((item) => (
             <ListItem key={item.slug} item={item} onRemove={onRemove} />
           ))}
         </div>
       </div>
-      <footer>Total: ${items.reduce((total, item) => total + Number(item.showbag_price), 0)}</footer>
-    </div>
+      {items.length > 0 && (
+        <footer className="container mx-auto px-6 py-3 border-t border-gray-400 font-bold text-lg text-center">
+          Total: ${items.reduce((total, item) => total + Number(item.showbag_price), 0)}
+        </footer>
+      )}
+    </section>
   );
 };
 
