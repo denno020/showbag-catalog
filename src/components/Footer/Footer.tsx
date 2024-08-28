98a95c22b837ee8b09d178d75b4a7abf72eb9a8f
Luke Denton <denno020@gmail.com>
2024-08-29 00:07:32 +0930

    Add note to the footer that prices are only a reference

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,6 +12,9 @@ const Footer = () => {
           <div className="flex w-full lg:w-1/2 ">
             <div className="px-3 md:px-0">
               <h3 className="font-bold text-gray-900">About</h3>
+              <p>
+                <em>Prices are for reference only</em>
+              </p>
               <p className="py-4">
                 Find this app useful and want to say thanks? In honour of Steptember, and all the steps you'll be taking
                 to pick up your showbags, please consider donating to the{' '}
