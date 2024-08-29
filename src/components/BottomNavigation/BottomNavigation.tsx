03465e3c1b961edfcd131132fce87bb65b5a0498
Luke Denton <denno020@gmail.com>
2024-08-29 22:43:22 +0930

    Fix SVG attribute issues

--- src/components/BottomNavigation/BottomNavigation.tsx
+++ src/components/BottomNavigation/BottomNavigation.tsx
@@ -37,7 +37,7 @@ const BottomNavigation = () => {
       <BottomNavigationDaisyUI.Item active={location === '/list'} onClick={handleNavigateToList}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="currentColor" viewBox="0 0 16 16">
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2"
           />
         </svg>
