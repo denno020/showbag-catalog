22822551b7c7879256b2d60a06bb99d75d8f5da2
Luke Denton <denno020@gmail.com>
2024-08-31 13:06:10 +0930

    Add some basic animations to collect/remove list item actions
    I know that I've implemented the setTimeouts in a less than optimal way.. How could I possibly regret that..

--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'wouter';
 import classnames from 'classnames';
 import { Badge, Button } from 'react-daisyui';
@@ -14,10 +15,39 @@ const ListItem = (props: ListItemProps) => {
   const toggleInList = useStore((state) => state.toggleInList);
   const toggleBagCollected = useStore((state) => state.toggleBagCollected);
   const collectedBags = useStore((state) => state.collectedBags);
+  const hideCollected = useStore((state) => state.listOptions.hideCollected);
   const { image, title, showbag_price, slug, showbag_stalls } = item;
+  const [isRemoved, setIsRemoved] = useState(false);
+  const [isCollected, setIsCollected] = useState(false);
+  const isBagCollected = collectedBags.includes(item.slug);
+
+  const handleCollect = () => {
+    if (isBagCollected || !hideCollected) {
+      toggleBagCollected(item.slug);
+      return;
+    }
+
+    setIsCollected(true);
+    setTimeout(() => {
+      toggleBagCollected(item.slug);
+    }, 300);
+  };
+
+  const handleRemove = () => {
+    setIsRemoved(true);
+    setTimeout(() => {
+      toggleInList(slug);
+    }, 300);
+  };
 
   return (
-    <div className={classes.listItem}>
+    <div
+      className={classnames(
+        classes.listItem,
+        { [classes.listItemRemoved]: isRemoved },
+        { [classes.collectedAnimation]: isCollected }
+      )}
+    >
       <div>
         <Link to={slug} state={{ internalLink: true }}>
           <img src={image.permalink} alt="Showbag preview" className="w-28 aspect-auto" />
@@ -43,10 +73,7 @@ const ListItem = (props: ListItemProps) => {
             </div>
           </div>
           <div className="flex gap-2 justify-end">
-            <Button
-              onClick={() => toggleBagCollected(item.slug)}
-              className={classnames({ 'bg-green-500': collectedBags.includes(item.slug) })}
-            >
+            <Button onClick={handleCollect} className={classnames({ 'bg-green-500': isBagCollected })}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
                 <path
                   fillRule="evenodd"
@@ -56,11 +83,7 @@ const ListItem = (props: ListItemProps) => {
               </svg>
               <span className="sr-only">Mark collected</span>
             </Button>
-            <Button
-              onClick={() => {
-                toggleInList(slug);
-              }}
-            >
+            <Button onClick={handleRemove}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
