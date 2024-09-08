e58774db99f531ff956c1c5cb441a2a37f5585d9
Luke Denton <denno020@gmail.com>
2024-09-08 22:04:42 +0930

    Add note regarding the show being completed, extract DonationLink

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import ProductProvider from './components/Product/ProductProvider';
 import Search from './components/Search';
 import Footer from './components/Footer';
 import Logo from './components/Logo/Logo.tsx';
+import DonationLink from './components/DonationLink/DonationLink.tsx';
 import { useStore } from './store/useStore.ts';
 import { useShowbags } from './hooks/useShowbags.ts';
 import { useList } from './hooks/useList.ts';
@@ -61,6 +62,35 @@ const App = (props: { showbags: ShowbagItem[] }) => {
               click here
             </a>
           </p>
+        </div>
+        <div className="container mx-auto py-4">
+          <div
+            className="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+            role="alert"
+          >
+            <div className="flex">
+              <div className="py-1">
+                <svg
+                  className="fill-current h-6 w-6 text-teal-500 mr-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                </svg>
+              </div>
+              <div>
+                <p className="font-bold">The Royal Adelaide Show has come to a close for 2024</p>
+                <p className="text-sm">
+                  I will leave this site up and running for future reference (and maybe future improvements upon the
+                  official site!)
+                </p>
+                <p className="py-4">
+                  As a reminder, if you found this app useful and want to say thanks, please consider donating to the{' '}
+                  <DonationLink>Cerebal Palsy Alliance</DonationLink>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </Route>
       <section>
